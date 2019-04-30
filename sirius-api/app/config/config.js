33c9aa5f3192989bb80b9497137f7e62a54dc73c
Igor Aparecido Gomes de Oliveira <igorgomes96@hotmail.com>
2019-04-29 22:31:14 -0300

    Atualização

--- sirius-api/app/config/config.js
+++ sirius-api/app/config/config.js
@@ -1,5 +1,5 @@
 module.exports = {
-    connectionStringProd: 'mongodb://sirius:[jZ[*2cdZ.4I@kamino.mongodb.umbler.com:53768/sirius',
+    connectionStringProd: 'mongodb://sirius:[jZ[*2cdZ.4I@mongo_sirius:27017/sirius',
     connectionStringDev: 'mongodb://localhost:27017/sirius',
     productionMode: true
 }
